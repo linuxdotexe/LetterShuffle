e2d6929ec62fa20f735453ed4aea2b01ad63d8a5
sai nivas mangu <sainonbeat99@gmail.com>
2024-10-24 17:06:43 +0530

    fix: hide unreleased

--- app/api/watchlist/route.ts
+++ app/api/watchlist/route.ts
@@ -2,16 +2,15 @@ import * as cheerio from "cheerio";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-
+  const headers = new Headers();
+  headers.set("Cookie", "filmFilter=hide-unreleased");
   // fetching the first page
   const username = searchParams.get("username");
   const url = "https://letterboxd.com/" + username + "/watchlist";
   try {
     const response = await fetch(`${url}`, {
       method: "GET",
-      headers: {
-        "Content-Type": "text/html",
-      },
+      headers: headers,
       next: { revalidate: 3600 },
     });
 
@@ -36,13 +35,13 @@ export async function GET(request: Request) {
       const $$ = cheerio.load(pageHtml);
 
       $$(".poster").each((index, element) => {
-        const slug = $$(element).attr("data-film-slug") as string;
+        const slug = $$(element).find("img").attr("alt") as string;
         data.push(slug); // Adjust index based on the page number
       });
     }
     // fetch each page and load up data
     $("div.poster").each((index, element) => {
-      const slug = $(element).attr("data-film-slug") as string;
+      const slug = $(element).find("img").attr("alt") as string;
       data[index] = slug;
     });
 

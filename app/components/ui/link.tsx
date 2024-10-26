ade85c324d89179e567fb145e4dc83337618b67a
sai nivas mangu <sainonbeat99@gmail.com>
2024-10-26 08:54:11 +0530

    add: link component

--- app/components/ui/link.tsx
+++ app/components/ui/link.tsx
@@ -1,19 +1,48 @@
 import { ReactNode } from "react";
+import Link from "next/link";
 
 interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children: ReactNode;
   href: string;
+  isExternal?: boolean;
   props?: LinkProps;
+  small?: boolean;
 }
 
-export default function Link({ children, href, props }: LinkProps) {
+export default function Anchor({
+  children,
+  href,
+  isExternal = false,
+  className,
+  small = false,
+  props,
+}: LinkProps) {
   return (
-    <a
-      className="h-fit rounded-md bg-green-500 px-2 py-1 text-base font-bold text-slate-950 hover:bg-green-300"
+    <Link
+      className={`group inline-flex items-center font-medium ${small ? "text-sm lg:text-base" : "text-base lg:text-lg"} ${className}`}
       href={href}
       {...props}
     >
       {children}
-    </a>
+      {isExternal ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={`${small ? "20" : "24"}`}
+          height={`${small ? "20" : "24"}`}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="transition-transform group-hover:animate-pulse"
+        >
+          <path d="M7 7h10v10" />
+          <path d="M7 17 17 7" />
+        </svg>
+      ) : (
+        <></>
+      )}
+    </Link>
   );
 }
